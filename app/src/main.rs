019f3c8e84d3ace6752bb6438513687f560aa04c
Derrick Plotsky <derrickplotsky@gmail.com>
2024-02-28 20:58:52 -0800

    An app
    It just calls one thing, and this isn't what the actual app will be,
    but at least it does something.

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use birders::{models::regions::RegionType, Birders, Credentials};
+
+#[tokio::main]
+async fn main() {
+    let api_token = std::env::var("EBIRD_API_KEY").unwrap();
+    let credentials = Credentials { api_token };
+
+    let birders = Birders::new(credentials);
+    let sub_regions = birders
+        .sub_region_list("CA", &RegionType::Subnational1)
+        .await
+        .map_err(|e| e.to_string())
+        .unwrap();
+    let serialized = serde_json::to_string(&sub_regions).unwrap();
+    println!("{serialized}");
 }
