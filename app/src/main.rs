cced1a999bafd921dad49371c69f51cfd2ceb75f
Derrick Plotsky <derrickplotsky@gmail.com>
2024-05-13 19:46:48 -0700

    Update runner for newest code

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,21 +1,58 @@
-use birders::{models::regions::RegionType, Birders, Credentials};
+use birders::{api::hotspots::NearbyParams, models::regions::RegionType, Birders, Credentials};
 
 #[tokio::main]
 async fn main() {
     let api_token = std::env::var("EBIRD_API_KEY").unwrap();
     let credentials = Credentials { api_token };
 
-    let birders = Birders::new(credentials);
+    println!("starting");
+
+    let birders = if std::env::var("DEBUG_PRINT").is_ok() {
+        Birders::new_with_debug_printing(credentials)
+    } else {
+        Birders::new(credentials)
+    };
     let sub_regions = birders
-        .sub_region_list("CA", RegionType::Subnational1)
+        .sub_region_list("CA-BC", RegionType::Subnational2)
         .get()
         .await
         .unwrap();
+
+    println!("have subregion list");
     let region_info = birders
         .region_info(&sub_regions.get(1).unwrap().code)
         .get()
         .await
         .unwrap();
-    let serialized = serde_json::to_string(&region_info).unwrap();
+
+    println!("have region info");
+
+    let hotspots_in_region = birders
+        .hotspots_in_region(&region_info.code, None)
+        .get()
+        .await
+        .unwrap();
+    let hotspot = birders
+        .hotspot_info(&hotspots_in_region.first().unwrap().loc_id)
+        .get()
+        .await
+        .unwrap();
+    let serialized = serde_json::to_string(&hotspot).unwrap();
+    println!("{serialized}");
+
+    let nearby_hotspots = birders
+        .nearby_hotspots(
+            48.387562021244186,
+            -123.72295129044818,
+            Some(NearbyParams {
+                back: Some(1),
+                dist: None,
+            }),
+        )
+        .get()
+        .await
+        .unwrap();
+
+    let serialized = serde_json::to_string(&nearby_hotspots).unwrap();
     println!("{serialized}");
 }
